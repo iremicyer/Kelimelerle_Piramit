a209f1bb47a61ec21d1e83a1d126f805a894a712
İREM İÇYER <62353089+iremicyer@users.noreply.github.com>
2020-09-01 22:47:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace Kelimeler_ile_Piramit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mesajinizi giriniz:");
+            string mesaj = Console.ReadLine();
+
+            string goster = "";
+            string piramit = "*";
+
+            for (int i = 0; i < mesaj.Length; i++)
+            {
+                goster += mesaj[i];
+
+                for (int j = 0; j < mesaj.Length; j++)
+                {
+                    if ((mesaj.Length - goster.Length) / 2 == j) // Döngü orta sayıya ulaştığında elimizdeki goster isimli kelimeyi ekliyoruz... 
+                    {
+                        piramit += goster; 
+                        j += goster.Length; // kelime uzunluğumuz kadar 2. döngümüzün sayacını artrıyoruz ki fazlada yıldız eklemesin...
+                    }
+                    piramit += "*"; // Eğer kelime eklemiyorsak yıldız ekliyoruz... 
+                }
+                Console.WriteLine(piramit); // Ekranda gösteriyoruz... 
+                piramit = "*"; // diğer saatır da yine piramiti sıfırlıyoruz ki bir önceki satır ile karışmasın... 
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
+
